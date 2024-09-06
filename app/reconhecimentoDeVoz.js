e5ab66cd0893509fbdbeabe7fbacf48705a9eb35
Vini <134401761+vini-destruidor19@users.noreply.github.com>
2024-09-06 10:39:33 -0300

    Add files via upload

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,15 +1,24 @@
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
+const elementChute = document.getElementById("chute")
+
 const recognition = new SpeechRecognition();
 recognition.lang = "pt-br"
 recognition.start()
 
 recognition.addEventListener("result",onSpeaK)
+
 function onSpeaK(e){
-    console.log(e.results[0],[0.].transcript)
+    e.log(e.results[0],[0.].transcript)
+    exibeChute(chute)
 }
 
-
+function exibeChute(chute){
+  elementChute.innerHTML = `
+  <div>Você disse: </div>
+  <span> class="box">${chute}</span>
+  `
+}
 
 
